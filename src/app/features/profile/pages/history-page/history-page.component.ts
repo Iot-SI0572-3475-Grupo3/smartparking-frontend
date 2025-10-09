17df065a90f5294592c76bbb19b7a34e933770b5
Fernando Salgado <fs142610@gmail.com>
2025-10-08 22:01:03 -0500

    feat(profile): create profile pages for settings and history

---
+++ src/app/features/profile/pages/history-page/history-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ReservationHistoryComponent } from '../../components/reservation-history/reservation-history.component';
+import { NavbarComponent} from "../../../../layout/navbar/navbar.component";
+
+@Component({
+  selector: 'app-history-page',
+  standalone: true,
+  imports: [ReservationHistoryComponent, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <app-reservation-history></app-reservation-history>
+  `
+})
+export class HistoryPageComponent {}
