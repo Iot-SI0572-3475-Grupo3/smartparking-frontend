df372fc0bc07b39ba86f15f89a53035908696b95
Fernando Salgado <fs142610@gmail.com>
2025-11-09 14:55:28 -0500

    feat(profile): updated profile settings logic

--- src/app/features/profile/pages/profile-settings/profile-settings.component.ts
+++ src/app/features/profile/pages/profile-settings/profile-settings.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { PushNotificationsComponent } from '../../components/push-notifications/push-notifications.component';
-import {NavbarComponent} from "../../../../layout/navbar/navbar.component";
 
 @Component({
   selector: 'app-profile-settings',
   standalone: true,
-  imports: [PushNotificationsComponent, NavbarComponent], // ← Agregar
+  imports: [PushNotificationsComponent], // ← Quitar NavbarComponent
   template: `
-    <app-navbar></app-navbar>
     <app-push-notifications></app-push-notifications>
   `
 })
