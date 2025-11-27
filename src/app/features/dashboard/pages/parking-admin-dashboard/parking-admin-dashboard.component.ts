dfe2017793c5102e8199ae67f36040a55d04e8a8
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2025-11-26 19:52:18 -0500

    feat: navbar components unificated and

--- src/app/features/dashboard/pages/parking-admin-dashboard/parking-admin-dashboard.component.ts
+++ src/app/features/dashboard/pages/parking-admin-dashboard/parking-admin-dashboard.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import {ParkingAdminNavbarComponent} from "../../../../layout/navbar-parking-admin/parking-admin-navbar.component";
 
 @Component({
   selector: 'app-parking-admin-dashboard',
   standalone: true,
-  imports: [
-    ParkingAdminNavbarComponent
-  ],
+  imports: [ ],
   templateUrl: './parking-admin-dashboard.component.html',
   styleUrl: './parking-admin-dashboard.component.scss'
 })
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { AuthenticationService} from "../../features/iam/services/authentication.service";
+import { Observable } from 'rxjs';
+
+type UserRole = 'university_member' | 'administrator';
 
 @Component({
   selector: 'app-navbar',
@@ -12,19 +16,64 @@ import { RouterModule } from '@angular/router';
 export class NavbarComponent {
   // Estado del menú móvil
   isMobileMenuOpen = false;
+  isAvatarMenuOpen = false;
+
+  user_role = this.authenticationService.currentUserRole as Observable<UserRole>;
 
   // Navegación del navbar
-  navItems = [
-    { label: 'Inicio', route: '/dashboard' },
-    { label: 'Reservas', route: '/reservations' },
-    { label: 'Historial', route: '/history' },
-    { label: 'Perfil', route: '/profile' }
-  ];
+  navItems: Record<UserRole, { label: string; route: string }[]> = {
+    university_member: [
+      { label: 'Inicio', route: '/dashboard' },
+      { label: 'Reservas', route: '/reservations' },
+      { label: 'Historial', route: '/history' },
+      { label: 'Perfil', route: '/profile' }
+    ],
+    administrator: [
+      { label: 'Panel', route: '/admin/dashboard' },
+      { label: 'Usuarios', route: '/admin/users-panel' },
+      { label: 'Reportes', route: '/admin/reports' },
+      { label: 'Configuración', route: '/admin/configuration' }
+    ]
+  };
 
-  // Método para manejar clics en navegación
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private el: ElementRef
+  ) {  }
+
+
+
+
+  // Metodo para manejar clics en navegación
   onNavClick(route: string) {
     // Aquí podrías agregar lógica adicional si es necesario
     console.log('Navegando a:', route);
+  }
+
+  toggleAvatarMenu(event?: MouseEvent) {
+  event?.stopPropagation();
+  this.isAvatarMenuOpen = !this.isAvatarMenuOpen;
+  }
+
+  closeAvatarMenu() {
+    this.isAvatarMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.closeAvatarMenu();
+    }
+  }
+
+  logout(){
+    this.authenticationService.logOut();
+
+
+    this.router.navigate(['/login']).then();
+    this.closeAvatarMenu();
   }
 
   // Toggle del menú móvil
