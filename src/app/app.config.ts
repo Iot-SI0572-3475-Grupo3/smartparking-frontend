440ad3378914b3fb8b2905be3704a457f3d79407
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2025-11-29 16:08:56 -0500

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,22 +5,12 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authenticationInterceptor} from "./features/iam/services/authentication.interceptor";
-import {AuthenticationService} from "./features/iam/services/authentication.service";
 
-export function initAuth() {
-  const authService = inject(AuthenticationService);
-  return () => authService.initSession();
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authenticationInterceptor])),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initAuth,
-      multi: true
-    }
   ]
 };
